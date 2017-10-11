4ef26478619f081c12dd06facd228d6ebca22293
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-11 10:13:14 -0700

    Express Lessons

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+
+const parser = require('body-parser')
+app.use(parser.json())
+
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Server party on port ${port}!`)
 app.listen(port, listener)
+
+app.get('/ping', (req, res) => {
+  const message = 'Pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.setHeader('content-type', 'text/html')
+  res.send(`<h1>${message}</h1>`)
+})
+
+app.post('/my-post-route', (req, res) => {
+  res.send(`You hit /my-post-route with the following message: ${req.body.message}`)
+})
